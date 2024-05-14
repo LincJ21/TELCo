52ccf38f16c40df5ace2558cf4d3b948bd08d0e2
Josué de Jesús Torres Torres <141877411+LincJ21@users.noreply.github.com>
2024-05-13 23:16:50 -0500

    01

--- TELCo/app.py
+++ TELCo/app.py
@@ -70,6 +70,6 @@ def base(request: Request):
 def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
-if __name__ == "__main__":
+if __name__ == "__app__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8080)
